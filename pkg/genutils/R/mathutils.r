a44b92f9792f348717ecaf948f5c77d7aff01d27
jaunario (Jorrel Khalil Aunario) <jaunario@gmail.com>
2012-07-13 05:29:00 +0000

    Added in.range function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rodbcext@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/genutils/R/mathutils.r
+++ pkg/genutils/R/mathutils.r
@@ -6,3 +6,17 @@
 rescale <- function(x, oldmin, oldmax, newmin, newmax){
   return((x-oldmin)*(newmax-newmin)/(oldmax-oldmin) + newmin)
 }
+
+
+in.range <- function(x, minofrange, maxofrange, min.inc=TRUE, max.inc=TRUE){
+  if(minofrange>maxofrange){
+    temp <- minofrange
+    minofrange <- maxofrange
+    maxofrange <- temp
+  }
+  uop <- ifelse(max.inc, "<=", "<")
+  lop <- ifelse(min.inc, ">=", ">")
+  
+  return(get(lop)(x,minofrange) & get(uop)(x, maxofrange))
+  
+}
